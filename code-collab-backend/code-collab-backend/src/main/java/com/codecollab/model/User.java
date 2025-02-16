d23c691353682c6b048d81f73fa29a9f24e37eff
GiharaNavindu <raviherath1961@gamil.com>
2025-02-16 16:23:41 +0530

    User edit

--- code-collab-backend/code-collab-backend/src/main/java/com/codecollab/model/User.java
+++ code-collab-backend/code-collab-backend/src/main/java/com/codecollab/model/User.java
@@ -1,13 +1,42 @@
+// package com.codecollab.model;
+
+
+// public class User {
+//     private String username;
+//     private String roomId;
+
+//     public User(String username, String roomId) {
+//         this.username = username;
+//         this.roomId = roomId;
+//     }
+
+//     public String getUsername() {
+//         return username;
+//     }
+
+//     public String getRoomId() {
+//         return roomId;
+//     }
+
+//     public Object getSocket() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+// }
+
+
 package com.codecollab.model;
 
+import java.io.PrintWriter;
 
 public class User {
     private String username;
     private String roomId;
+    private PrintWriter clientOutputStream;  // This will allow communication with the client
 
-    public User(String username, String roomId) {
+    public User(String username, String roomId, PrintWriter clientOutputStream) {
         this.username = username;
         this.roomId = roomId;
+        this.clientOutputStream = clientOutputStream;
     }
 
     public String getUsername() {
@@ -16,5 +45,9 @@ public class User {
 
     public String getRoomId() {
         return roomId;
+    }
+
+    public PrintWriter getClientOutputStream() {
+        return clientOutputStream;
     }
 }
